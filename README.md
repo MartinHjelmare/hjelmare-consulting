a1c10d391b3212bb6f1160612b8634486ef63469
Martin Hjelmare <marhje52@gmail.com>
2022-06-01 14:33:05 +0200

    Add new site

---
+++ README.md
@@ -0,0 +1,51 @@
+# Hjelmare Consulting Site
+
+## Installation
+
+1. Clone the repo
+
+    ```sh
+    git clone https://github.com/MartinHjelmare/hjelmare-consulting.git
+    ```
+
+2. Open the project folder
+
+    ```sh
+    cd hjelmare-consulting
+    ```
+
+3. Install packages and dependencies
+
+    ```sh
+    yarn
+    ```
+
+    NOTE: If you don't have yarn installed, you can install it globally using npm:
+
+    ```sh
+    npm install --global yarn
+    ```
+
+4. Start a local dev server at `http://localhost:3000`
+
+    ```sh
+    yarn dev
+    ```
+
+## For production build and generating static files
+
+### Build for production and launch server
+
+```sh
+yarn build
+```
+
+```sh
+yarn start
+```
+
+### Generate static project
+
+```sh
+yarn generate
+```
---
+++ store/index.js
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const state = () => ({
+  projectsHeading: "Projects Portfolio",
+  projectsDescription: "Some of the projects I have successfully completed",
+  projects: [],
+  clientsHeading: "Some of the brands I worked with",
+  clients: [],
+  aboutMe: [
+    {
+      id: uuidv4(),
+      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+    },
+    {
+      id: uuidv4(),
+      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+    },
+  ],
+  copyrightDate: new Date().getFullYear(),
+  socialProfiles: [
+    {
+      id: uuidv4(),
+      name: "GitHub",
+      icon: "github",
+      url: "https://github.com/MartinHjelmare",
+    },
+    {
+      id: uuidv4(),
+      name: "Twitter",
+      icon: "twitter",
+      url: "https://twitter.com/martinhjelmare",
+    },
+  ],
+  categories: [
+    {
+      id: uuidv4(),
+      value: "web",
+      name: "Web Application",
+    },
+    {
+      id: uuidv4(),
+      value: "mobile",
+      name: "Mobile Applicaiton",
+    },
+    {
+      id: uuidv4(),
+      value: "ui-ux",
+      name: "UI/UX Design",
+    },
+    {
+      id: uuidv4(),
+      value: "branding",
+      name: "Branding & Animations",
+    },
+  ],
+});
+
+export const getters = {
+  getProjectById: (state) => (id) => {
+    return state.projects.find((project) => project.id == id);
+  },
+};
+
+export const mutations = {
+  // @todo
+};
+
+export const actions = {
+  // @todo
+};
